597fd73d5edcd1631b72eada3924aabbe82319ea
laislopes <laisjkl@hotmail.com>
2023-05-18 11:19:27 -0300

    Implements CRUD.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const books = [
     {id: 1, "title": "Harry Potter"},
     {id: 2, "title": "The Hobbit"}
@@ -14,5 +16,32 @@ app.get('/', (req, res) => {
 app.get('/books', (req, res) => {
     res.status(200).json(books)
 })
+
+app.get('/books/:id', (req, res) =>{
+    let index = findBook(req.params.id);
+    res.json(books[index]);
+})
+
+app.post('/books', (req, res) =>{
+    books.push(req.body);
+    res.status(201).send('The book has been registered successfully!')
+})
+
+app.put('/books/:id', (req, res) =>{
+    let index = findBook(req.params.id);
+    books[index].title = req.body.title;
+    res.json(books);
+})
+
+app.delete('/books/:id', (req, res) =>{
+    let {id} = req.params;
+    let index = findBook(id);
+    books.splice(index, 1);
+    res.send(`The book ${id} has been deleted successfully!`)
+})
+
+function findBook(id){
+    return books.findIndex(book => book.id == id);
+}
 
 export default app
