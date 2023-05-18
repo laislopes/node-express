827c2f0d29bd21d9aec0851c029ec72661bf9b07
laislopes <laisjkl@hotmail.com>
2023-05-18 10:46:26 -0300

    Add express framework in the project.

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
 
-const routes = {
-    '/': 'Node Course',
-    '/books': 'Books route'
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(routes[req.url]);
-})
-
-server.listen(port, ()=> {
+app.listen(port, ()=> {
     console.log(`Server listening on http://localhost:${port}`)
 })
