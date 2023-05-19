456c5821df9d0cdf31ca95b6d7081e871bbc3071
laislopes <laisjkl@hotmail.com>
2023-05-19 12:11:55 -0300

    Implements Authors CRUD.

---
+++ src/routes/authorsRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import AuthorController from "../controllers/authorsController.js";
+
+const router = express.Router();
+
+router
+    .get("/authors", AuthorController.getAll)
+    .get("/authors/:id", AuthorController.getById)
+    .post("/authors", AuthorController.add)
+    .put("/authors/:id", AuthorController.update)
+    .delete("/authors/:id", AuthorController.delete);
+
+export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import books from "./booksRoutes.js";
+import authors from "./authorsRoutes.js";
 
 const routes = (app) => {
     app.route('/').get((_,res) => {
@@ -8,9 +9,9 @@ const routes = (app) => {
 
     app.use(
         express.json(),
-        books
+        books,
+        authors
     )
 };
-
 
 export default routes;
